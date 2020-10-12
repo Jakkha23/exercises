8cf481fe1cbf335f44f0acfd48ca50fdf2331f6d
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2020-10-12 14:23:29 +0200

    solve 10

---
+++ A-Beginner/10/script.js
@@ -0,0 +1,10 @@
+//konstaterar variabler till olika strängar
+let x = "jag"
+let y = " heter"
+let z = " Jakob"
+
+//lägger ihop alla strängar
+const stringAdder = x+y+z
+
+//loggar resultatet (använd "node 10/script.js" i konsolen)
+console.log(stringAdder)
